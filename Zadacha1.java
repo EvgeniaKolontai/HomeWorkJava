0edbe2ba33adbd8dd9b5c6ef2f328b04b3f67562
Evgenia Kolontai <evgeniakolontai@MacBook-Air-Evgenia.local>
2023-01-14 20:25:33 +0400

    First task

---
+++ Zadacha1.java
@@ -0,0 +1,9 @@
+public class Zadacha1 {
+    public static void main (String args[]){
+        int a =10;
+        int b =2000;
+        int x = a+(int)(Math.random()*b);
+        System.out.println("Random namber:"+ x);
+    }
+    
+ }
