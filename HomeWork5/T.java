10242e0eba607e562aa893abc38345ddc7e55a55
Evgenia Kolontai <evgeniakolontai@MacBook-Air-Evgenia.local>
2023-01-30 21:17:08 +0400

    Task

--- HomeWork5/T.java
+++ HomeWork5/T.java
@@ -1,67 +1,34 @@
 
-import java.util.ArrayList;
-import java.util.Collections;
+// Создать словарь HashMap. Обобщение <Integer, String>.
+// Заполнить пятью ключами (индекс, ФИО+Возраст+Пол "Иванов Иван Иванович 28 м"), добавить ключь, если не было!)
+// Изменить значения сделав пол большой буквой.
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 import java.util.Set;
-public class Task4 {
-//    1. Создать словарь HashMap. Обобщение <Integer, String>.
-//    2. Заполнить пятью ключами (индекс, ФИО+Возраст+Пол "Иванов Иван Иванович 28 м"), добавить ключь, если не было!)
-public static void main(String[] args) throws Exception {
-    Map<Integer, String> hashMap = new HashMap<>();
-    hashMap.putIfAbsent(1, "Иванов Иван Иванович 28 м ");
-    hashMap.putIfAbsent(2, "Петрова Екатерина Петровна 13 ж");
-    hashMap.putIfAbsent(3, "Смирнов Константин игоревич 20 м");
-    hashMap.putIfAbsent(4, "Суркова Анна Владимировна 30 ж");
-    hashMap.putIfAbsent(5, "Сидоров Артем Сергеевич 22 м");
 
-    for (Map.Entry<Integer, String> iterable_element : hashMap.entrySet()) {
-        System.out.println(iterable_element.getKey()+" "+ iterable_element.getValue());
-    } 
+public class Task123 {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.putIfAbsent(1, "Иванов Иван Иванович 28 м ");
+        hashMap.putIfAbsent(2, "Петрова Екатерина Петровна 13 ж");
+        hashMap.putIfAbsent(3, "Смирнов Константин игоревич 20 м");
+        hashMap.putIfAbsent(4, "Суркова Анна Владимировна 30 ж");
+        hashMap.putIfAbsent(5, "Сидоров Артем Сергеевич 22 м");
+        // System.out.println(hashMap.toString());
 
-    ArrayList<Integer> age = new ArrayList<>();
-    ArrayList<String> family = new ArrayList<>();
-    ArrayList<String> name = new ArrayList<>();
-    ArrayList<String> soname = new ArrayList<>();
-    LinkedList<Integer> index = new LinkedList<>();
+        Set<Integer> keySet = hashMap.keySet();
+        // String sValues= hashMap.values();
 
-    System.out.println("\n 3.Изменить значения сделав пол большой буквой.");
-    Set<Integer> keySet = hashMap.keySet();
-    for (int i = 0; i < keySet.size(); i++) {
-        String [] string = (hashMap.get(keySet.toArray()[i]).split("\n"));   
-        for (int j = 0; j < string.length; j++) {
-            String [] tmp = string[j].split(" ");
-            System.out.println(tmp[0]+ " " + tmp[1] + " "+ tmp[2] +" "+ tmp[3]+" "+tmp[4].toUpperCase());
-        } 
+        for (Integer i: keySet) {
+            String sValues = hashMap.get(keySet.toArray()[i - 1]);
+            String[] newA = sValues.split(" ");
+            newA[newA.length-1]= newA[newA.length-1].toUpperCase();
+            String vl= "";
+            for(int k = 0; k<newA.length; k++){
+                if (k<newA.length-1)vl+=newA[k]+" ";
+                else vl+=newA[k];
+            }  hashMap.replace(i, sValues, vl);
+        }
+       System.out.println(hashMap);
     }
 
-    for (int i = 0; i < keySet.size(); i++) {
-        String [] string = (hashMap.get(keySet.toArray()[i]).split("\n"));   
-        for (int j = 0; j < string.length; j++) {
-            String [] tmp = string[j].split(" ");
-            index.add(i);
-            age.add(Integer.valueOf(tmp[3]));
-            family.add(tmp[0]+" ");
-            name.add(tmp[1].charAt(0) + ".");
-            soname.add(tmp[2].charAt(0) + ".");
-        } 
-    }
-
-    ArrayList<Integer> tmp = new ArrayList<>(age);
-    Collections.sort(tmp);
-    for (int i = 0; i < tmp.size(); i++) {
-        int j = age.indexOf(tmp.get(i));
-        index.set(i, j);
-    }
-    System.out.println("\n*5.Сортировать значения по возрасту и вывести отсортированную коллекция как в четвёртом пункте ");
-    for (int i = 0; i < index.size(); i++) {
-        System.out.printf(family.get(index.get(i)));
-        System.out.printf(name.get(index.get(i)));
-        System.out.printf(soname.get(index.get(i)));
-        System.out.println(" "+age.get(index.get(i)));
-      }
 }
-}
-
-    
